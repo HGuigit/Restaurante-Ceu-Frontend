f78927886e93699b8e409e58b7a0369015357687
marinachagasbg <69398287+marinachagasbg@users.noreply.github.com>
2022-07-12 15:29:56 -0300

    Add files via upload

---
+++ src/components/FormularioCadastroPedidos.jsx
@@ -0,0 +1,75 @@
+import {React, useState} from 'react'
+
+import styles from './LoginForm.module.css'
+
+import { Link, useNavigate } from 'react-router-dom'
+
+const CadastroPedidosForm = () => {
+
+  const [Comanda, setComanda] = useState('');
+  const [ValorTotal, setValorTotal] = useState('');
+  const [DataHora, setDataHora] = useState('');
+  const [Funcionario, setFuncionario] = useState('');
+  
+  const nav = useNavigate();
+
+  async function handleSignIn(e) {
+
+    e.preventDefault();
+
+  }
+
+  return (
+    <div>
+    <h1>Cadastro Pedido</h1>
+    <div className={styles.container}>
+      <form onSubmit={handleSignIn}>
+        <label>
+          <span>Comanda:</span>
+          <input
+            type="number"
+            placeholder="Comanda do pedido"
+            value={Comanda}
+            onChange={e => setComanda(e.target.value)}
+          />
+        </label>
+
+        <label>
+          <span>ValorTotal:</span>
+          <input
+            type="number"
+            placeholder="Valor do pedido"
+            value={ValorTotal}
+            onChange={e => setValorTotal(e.target.value)}
+          />
+        </label>
+
+        <label>
+          <span>Data:</span>
+          <input
+            type="datetime-local"
+            placeholder="Data e Hora"
+            value={DataHora}
+            onChange={e => setValorTotal(e.target.value)}
+          />
+        </label>
+
+        <label>
+          <span>Funcionario:</span>
+          <input
+            type="text"
+            placeholder="Funcionario"
+            value={Funcionario}
+            onChange={e => setValorTotal(e.target.value)}
+          />
+        </label>
+
+        <div className={styles.act}>
+          <button type="submit" className={styles.btn}>Confirmar Cadastro</button>
+          <Link to="/" className={styles.btn}> ???? </Link>
+        </div>
+      </form>
+    </div>
+  </div>
+  )
+}
